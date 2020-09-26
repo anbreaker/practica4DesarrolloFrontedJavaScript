30a96167233aca74d3c02035d03bd97388e28346
anbreaker <antunez19@gmail.com>
2020-09-26 21:30:02 +0200

    dropdown menu funcionando

--- templates/navbar.js
+++ templates/navbar.js
@@ -1,17 +1,24 @@
 export const templateNavbar = `
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="/">Práctica 4</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    
+      <div class="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Menu
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="/">Home</a>
+          <a class="dropdown-item" href="/login.html">Log In</a>
+          <a class="dropdown-item" href="/signup.html">Sign Up</a>
+        </div>
+      </div>
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ml-auto">
@@ -19,11 +26,12 @@ export const templateNavbar = `
           <a class="nav-link" href="/">Home<span class="sr-only"></span></a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link" href="/login.html">Login<span class="sr-only"></span></a>
+          <a class="nav-link" href="/login.html">Log In<span class="sr-only"></span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/signup.html">Sign Up</a>
         </li>
       </ul>
     </div>
-  </nav>`;
+  </nav>
+`;
