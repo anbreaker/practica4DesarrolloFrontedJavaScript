2aec929b4a7eb4b0db93b18e01d072f8f3c73aed
anbreaker <antunez19@gmail.com>
2020-09-26 12:42:07 +0200

    preventdefaul y submit puesto

--- js/app.js
+++ js/app.js
@@ -2,23 +2,42 @@ import {templateNavbar} from '../templates/navbar.js';
 import {templateFooter} from '../templates/footer.js';
 
 const main = () => {
-  const btnRegister = document.querySelector('#btn-register');
-  if (btnRegister) btnRegister.addEventListener('click', onClickRegister);
-
+  const formRegister = document.querySelector('#form-register');
   const customCheck = document.getElementById('customCheck1');
+
   if (customCheck) customCheck.checked = 0;
+
+  formRegister.addEventListener('submit', sendData);
 
   document.querySelector('.header-nav').innerHTML = templateNavbar;
   document.querySelector('.main').innerHTML = templateFooter;
 };
 
-const onClickRegister = () => {
+const sendData = (event) => {
+  event.preventDefault();
   const formRegister = document.querySelector('#form-register');
-  const inputsForm = [...formRegister.querySelectorAll('input')];
 
+  validateForm(formRegister);
+};
+
+const validateForm = (formRegister) => {
+  const inputsForm = [...formRegister.querySelectorAll('.form-control')];
   // console.log(inputsForm[11].checked);
   console.log(inputsForm);
-  inputsForm.forEach((item) => console.log(item));
+  inputsForm.forEach((item) => console.log(item.value));
+
+  try {
+    inputsForm.forEach((item) => {
+      if (!item.value) {
+        throw new Error(`${item.id} invalid`);
+      }
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    // formRegister.querySelector('.sms').innerHTML = 'sms';
+    return false;
+  }
 };
 
 document.addEventListener('DOMContentLoaded', main);
