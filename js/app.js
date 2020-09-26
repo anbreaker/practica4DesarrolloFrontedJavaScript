7efb3fae4b97ace0dd2f67ee87659d3a1e4c3d95
anbreaker <antunez19@gmail.com>
2020-09-26 19:21:45 +0200

    recogida de datos correcta, faltan validaciones y consultas de apis...

--- js/app.js
+++ js/app.js
@@ -3,41 +3,38 @@ import {templateFooter} from '../templates/footer.js';
 
 const main = () => {
   const formRegister = document.querySelector('#form-register');
-  const customCheck = document.getElementById('customCheck1');
-
-  if (customCheck) customCheck.checked = 0;
-
   formRegister.addEventListener('submit', sendData);
 
   document.querySelector('.header-nav').innerHTML = templateNavbar;
   document.querySelector('.main').innerHTML = templateFooter;
+
+  const countries = ['Spain', 'Portugal', 'France', 'Italy'];
+  let countriesSelect = '<option></option>';
+  countries.forEach((item) => (countriesSelect += `<option>${item}</option>`));
+
+  formRegister.querySelector('#exampleSelect').innerHTML = countriesSelect;
 };
 
 const sendData = (event) => {
   event.preventDefault();
+  const data = {};
   const formRegister = document.querySelector('#form-register');
-
-  validateForm(formRegister);
-};
-
-const validateForm = (formRegister) => {
   const inputsForm = [...formRegister.querySelectorAll('.form-input')];
-  // console.log(inputsForm[11].checked);
-  console.log(inputsForm);
-  inputsForm.forEach((item) => console.log(item.value));
 
-  try {
-    inputsForm.forEach((item) => {
-      if (!item.value) {
-        throw new Error(`${item.id} invalid`);
-      }
-    });
-    return true;
-  } catch (error) {
-    console.log(error);
-    // formRegister.querySelector('.sms').innerHTML = 'sms';
-    return false;
-  }
+  data.gender = inputsForm.filter((item) => item.checked)[0].value;
+  data.name = inputsForm[3].value;
+  data.surname = inputsForm[4].value;
+  data.email = inputsForm[5].value;
+  data.phone = inputsForm[6].value;
+  data.username = inputsForm[7].value;
+  data.password = inputsForm[8].value;
+  data.confirmPassword = inputsForm[9].value;
+  data.apiKey = inputsForm[10].value;
+  data.country = inputsForm[11].value;
+  data.textArea = inputsForm[12].value;
+  data.aceptTerms = inputsForm[13].checked;
+
+  console.log(data);
 };
 
 document.addEventListener('DOMContentLoaded', main);
