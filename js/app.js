96e76d2a039816e9b79b31fea62b0b003da10b3e
anbreaker <antunez19@gmail.com>
2020-09-27 14:19:44 +0200

    log funcionando

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import {templateNavbar} from '../templates/navbar.js';
 import {templateFooter} from '../templates/footer.js';
 
-const main = () => {
+function main() {
   const formRegister = document.querySelector('#form-register');
   const formLogin = document.querySelector('#form-login');
 
@@ -18,58 +18,78 @@ const main = () => {
 
     formRegister.querySelector('#exampleSelect').innerHTML = countriesSelect;
   }
-};
 
-const sendUser = (event) => {
-  event.preventDefault();
-  const user = {};
-  const formRegister = document.querySelector('#form-register');
-  const inputsForm = [...formRegister.querySelectorAll('.form-input')];
-  validateForm(inputsForm);
+  function sendUser(event) {
+    event.preventDefault();
+    const user = {};
+    // const formRegister = document.querySelector('#form-register');
+    const inputsForm = [...formRegister.querySelectorAll('.form-input')];
+    validateForm(inputsForm);
 
-  user.gender = inputsForm.filter((item) => item.checked)[0].value;
-  user.name = inputsForm[3].value;
-  user.surname = inputsForm[4].value;
-  user.email = inputsForm[5].value;
-  user.phone = inputsForm[6].value;
-  user.username = inputsForm[7].value;
-  user.password = inputsForm[8].value;
-  user.confirmPassword = inputsForm[9].value;
-  user.apiKey = inputsForm[10].value;
-  user.country = inputsForm[11].value;
-  user.textArea = inputsForm[12].value;
-  user.aceptTerms = inputsForm[13].checked;
+    user.gender = inputsForm.filter((item) => item.checked)[0].value;
+    user.name = inputsForm[3].value;
+    user.surname = inputsForm[4].value;
+    user.email = inputsForm[5].value;
+    user.phone = inputsForm[6].value;
+    user.username = inputsForm[7].value;
+    user.password = inputsForm[8].value;
+    user.confirmPassword = inputsForm[9].value;
+    user.apiKey = inputsForm[10].value;
+    user.country = inputsForm[11].value;
+    user.textArea = inputsForm[12].value;
+    user.aceptTerms = inputsForm[13].checked;
 
-  const dataBase = window.localStorage.getItem('users')
-    ? JSON.parse(window.localStorage.getItem('users'))
-    : [];
+    const dataBase = window.localStorage.getItem('users')
+      ? JSON.parse(window.localStorage.getItem('users'))
+      : [];
 
-  dataBase.push(user);
+    dataBase.push(user);
+    window.localStorage.setItem('users', JSON.stringify(dataBase));
 
-  window.localStorage.setItem('users', JSON.stringify(dataBase));
+    // Form = ''
+    inputsForm.forEach((item) => (item.value = ''));
 
-  // Borrar Form
-  console.log(user);
-};
+    alert('Register ok!');
 
-const onClickLogin = (event) => {
-  event.preventDefault();
+    console.log(user);
+  }
 
-  const dataBase = window.localStorage.getItem('users')
-    ? JSON.parse(window.localStorage.getItem('users'))
-    : [];
-  const formLogin = document.querySelector('#form-login');
-  const inputsLogin = [...formLogin.querySelector('#input-log')];
+  function onClickLogin(event) {
+    event.preventDefault();
+    const inputsLogin = [...formLogin.querySelectorAll('.form-input')];
+    validateForm(inputsLogin);
+    const userLog = {};
+    userLog.name = inputsLogin[0].value;
+    userLog.password = inputsLogin[1].value;
 
-  let index = dataBase.find((item) => {
-    item.name === inputsLogin[0];
-  });
-  console.log(index);
+    const dataBase = window.localStorage.getItem('users')
+      ? JSON.parse(window.localStorage.getItem('users'))
+      : [];
 
-  if (!validateForm) return;
+    let findUser = dataBase.find(
+      (item) => item.name.toLowerCase() === inputsLogin[0].value.toLowerCase()
+    );
 
-  console.log('ver click login');
-};
+    // Sacar esto....
+    if (!findUser) {
+      showSms('The data entry is not correct');
+    } else if (findUser.password !== inputsLogin[1].value) {
+      showSms('The data entry is not correct');
+    } else {
+      window.location = 'user.html';
+    }
+  }
+
+  function showSms(smsLog) {
+    let sms = '';
+    sms += `
+    <div class="form-control-lg text-danger ">
+      <p>${smsLog}</p>
+    </div>`;
+
+    formLogin.querySelector('#sms').innerHTML = sms;
+  }
+}
 
 const validateForm = (data) => {
   try {
@@ -89,6 +109,5 @@ document.addEventListener('DOMContentLoaded', main);
 
 // https://plataforma.keepcoding.io/courses/dearrollo-frontend-javasscript-9/lectures/24503547
 // min 30
-
-// https://plataforma.keepcoding.io/courses/dearrollo-frontend-javasscript-9/lectures/24335431
-// hora 3.52
+// https://plataforma.keepcoding.io/courses/dearrollo-frontend-javasscript-9/lectures/24385589
+// min 36
