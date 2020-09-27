fd48e1ed91d9d3a7b17aba632eee1fa7c99e9b36
anbreaker <antunez19@gmail.com>
2020-09-27 21:01:48 +0200

    regiones para select

---
+++ js/regionsConfig.js
@@ -0,0 +1,41 @@
+export const countries = [
+  {id: 1, name: 'Spain'},
+  {id: 2, name: 'Portugal'},
+  {id: 3, name: 'Other Places in the World'},
+];
+
+export const regionsSpain = [
+  {id: 1, name: 'Andalucía'},
+  {id: 2, name: 'Aragón'},
+  {id: 3, name: 'Principado de Asturias'},
+  {id: 4, name: 'Illes Balears'},
+  {id: 5, name: 'Canarias'},
+  {id: 6, name: 'Cantabria'},
+  {id: 7, name: 'Castilla y León'},
+  {id: 8, name: 'Castilla-La Mancha'},
+  {id: 9, name: 'Cataluña'},
+  {id: 10, name: 'Comunitat Valenciana'},
+  {id: 11, name: 'Extremadura'},
+  {id: 12, name: 'Galicia'},
+  {id: 13, name: 'Comunidad de Madrid'},
+  {id: 14, name: 'Región de Murcia'},
+  {id: 15, name: 'Comunidad Foral de Navarra'},
+  {id: 16, name: 'País Vasco'},
+  {id: 17, name: 'La Rioja'},
+  {id: 18, name: 'Ciudad Autónoma de Ceuta'},
+  {id: 19, name: 'Ciudad Autónoma de Melilla'},
+];
+
+export const regionsPortugal = [
+  {id: 1, name: 'Minho'},
+  {id: 2, name: 'Trás-os-Montes e Alto Douro'},
+  {id: 3, name: 'Douro Litoral'},
+  {id: 4, name: 'Beira Litoral'},
+  {id: 5, name: 'Beira Alta'},
+  {id: 6, name: 'Beira Baixa'},
+  {id: 7, name: 'Ribatejo'},
+  {id: 8, name: 'Estremadura'},
+  {id: 9, name: 'Alto Alentejo'},
+  {id: 10, name: 'Baixo Alentejo'},
+  {id: 11, name: 'Algarve'},
+];
