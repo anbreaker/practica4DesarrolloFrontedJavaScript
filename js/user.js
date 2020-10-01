1cc7de5399ee1702c7820612f9bfd6de0886ce6b
anbreaker <antunez19@gmail.com>
2020-10-01 13:02:38 +0200

    paginacion andando

--- js/user.js
+++ js/user.js
@@ -2,12 +2,28 @@ import {URL_API, API_KEY} from '../js/config.js';
 import {getUser} from './dataBase.js';
 
 function main() {
+  let page = 1;
   const btnApiPopularMovies = document.querySelector('.btn-popular-movies');
+  const prevPageBtn = document.querySelector('#prev-pagination');
+  const nextPageBtn = document.querySelector('#next-pagination');
 
   if (btnApiPopularMovies) btnApiPopularMovies.addEventListener('click', getMovies);
+  if (prevPageBtn) prevPageBtn.addEventListener('click', () => pagination(-1));
+  if (nextPageBtn) nextPageBtn.addEventListener('click', () => pagination(1));
+
+  function pagination(flag) {
+    page += flag;
+    getMovies();
+    document.querySelector('#prev-pagination').classList.remove('hiddenBtn');
+    console.log(page);
+    if (page === 1) document.querySelector('#prev-pagination').classList.add('hiddenBtn');
+  }
 
   function getMovies() {
-    fetch(URL_API + API_KEY)
+    let url = URL_API + API_KEY;
+    if (page > 1) url += `&page=${page}`;
+
+    fetch(url)
       .then((response) =>
         response.ok ? Promise.resolve(response) : Promise.reject(response)
       )
